4523d94568e550b21ba8860b06875393732d6823
Bree Latimer <breedevelops@breelatimer.tech>
2026-06-19 18:51:25 -0400

    Stylistic changes and refactoring, added Resume to navbar

--- Portfolio/obj/Release/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Release/net10.0/Portfolio.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4f7f668002adb827f8877c4f0dd11181b81c06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a077b55d9938bb82c063f682535cd686da3511b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
